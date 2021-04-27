b209a2ca0825867d45d097818f11d622b2c9ffa6
Vedang Mahajan <62505173+Vedang-Mahajan@users.noreply.github.com>
2021-04-27 16:53:40 +0530

    Add files via upload

---
+++ Random Workable Map/Random Workable Map - Pseudo Code.py
@@ -0,0 +1,29 @@
+"""
+Set the start cell
+Set the target cell
+
+mapFound = False
+
+def RecreateMap():
+    Generate the arena with random open and close blocks
+
+Loop until mapFound == True
+    Check the cells around the bot.
+
+    Add that cell co-ordinate to the map_cells dictionary with the value 1 if that cell is open or 0 if closed. (This will be done for all the possible cells that the bot can reach)
+
+    If current_cell in map_cells:
+        reroute to other cell if open
+    Else:
+        Go back
+
+    If Cell co-ordinates and blocked/closed data for all the cells == True:
+        If there are blocks adjacent to the blocks tracing from the start to target cell:
+            mapFound = True
+            Random Workable Map Generated!
+            break
+    Else:
+        There is no path available for the bot to travel
+        RecreateMap()
+
+"""
