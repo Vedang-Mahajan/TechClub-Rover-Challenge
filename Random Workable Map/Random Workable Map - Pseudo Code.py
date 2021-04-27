02ba1575e22a93df92a48583a3b5e454e9d78844
Vedang Mahajan <62505173+Vedang-Mahajan@users.noreply.github.com>
2021-04-27 16:57:37 +0530

    Add files via upload

--- Random Workable Map/Random Workable Map - Pseudo Code.py
+++ Random Workable Map/Random Workable Map - Pseudo Code.py
@@ -8,7 +8,7 @@ def RecreateMap():
     Generate the arena with random open and close blocks
 
 Loop until mapFound == True
-    Check the cells around the bot.
+    Check the cells around the bot using the '8x8 Arena' code.
 
     Add that cell co-ordinate to the map_cells dictionary with the value 1 if that cell is open or 0 if closed. (This will be done for all the possible cells that the bot can reach)
 
